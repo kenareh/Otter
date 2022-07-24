f8a900390c8c76ba5789d29135a06adaa27bce74
Hossein Kenareh <h.kenareh@gmail.com>
2022-07-25 00:03:05 +0430

    auth

--- Otter.HttpEndPoint/Controllers/AuthorizedController.cs
+++ Otter.HttpEndPoint/Controllers/AuthorizedController.cs
@@ -6,14 +6,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Otter.HttpEndPoint.Controllers
 {
-    [Authorize]
+    // [Authorize]
     [ApiController]
     public class AuthorizedController : ControllerBase
     {
         #region Properties
 
-        public string UserId => User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "";
+        public string UserId => "457ba2cc-9487-4d7f-9f28-bbfaae3cc121"; //todo
+
+        // public string UserId => User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "";
         public string ClientId => HttpContext.User.Claims.FirstOrDefault(c => c.Type == "client_id")?.Value ?? "";
+
         public List<string> Audiences => User.Claims.Where(p => p.Type == "aud").Select(p => p.Value).ToList();
         public List<string> Scopes => User.Claims.Where(p => p.Type == "scope").Select(p => p.Value).ToList();
 
